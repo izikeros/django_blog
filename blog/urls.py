07f30d8294968278d8654215be0a5f3c32c5416a
Krystian Safjan <krystian.safjan@wilabs.com>
2017-04-20 16:29:52 +0200

    forms added. experimenting with url names

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.post_list, name='post_list'),
-	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^$', views.post_list, name='name_post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='name_post_detail'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='name_post_edit'),
+    url(r'^post/new/$', views.post_new, name='name_post_new'),
 ]
